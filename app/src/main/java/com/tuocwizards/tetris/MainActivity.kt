6ed4c9e6c11f9b092756139766617f68fc177492
tUoCWizards <pkpbi01@mail.ru>
2022-01-27 22:00:09 +0300

    create main page

--- app/src/main/java/com/tuocwizards/tetris/MainActivity.kt
+++ app/src/main/java/com/tuocwizards/tetris/MainActivity.kt
@@ -2,10 +2,14 @@ package com.tuocwizards.tetris
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    //val button: Button = findViewById<Button>(R.id.btn_new_game)
+    //button.set
 }
