2b5c8782c202bb2b88f4c1873229a9077f2634b9
tUoCWizards <pkpbi01@mail.ru>
2022-01-27 22:51:53 +0300

    Listeners
    Add:
    onBtnNewGameClick
    onBtnResetScore
    onBtnExit
    Create GameActivity

--- app/src/main/java/com/tuocwizards/tetris/MainActivity.kt
+++ app/src/main/java/com/tuocwizards/tetris/MainActivity.kt
@@ -1,15 +1,38 @@
 package com.tuocwizards.tetris
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//import android.widget.Button
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
+    private var tvHighScore: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        val btnNewGame = findViewById<Button>(R.id.btn_new_game)
+        val btnResetScore = findViewById<Button>(R.id.btn_reset_score)
+        val btnExit = findViewById<Button>(R.id.btn_exit)
+        tvHighScore = findViewById(R.id.tv_high_score)
+
+        btnNewGame.setOnClickListener(this::onBtnNewGameClick)
+        btnResetScore.setOnClickListener(this::onBtnResetScore)
+        btnExit.setOnClickListener(this::onBtnExit)
     }
 
-    //val button: Button = findViewById<Button>(R.id.btn_new_game)
-    //button.set
+    private fun onBtnNewGameClick(view: View) {
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun onBtnResetScore(view: View) {}
+
+    private fun onBtnExit(view: View) {
+        exitProcess(0)
+    }
 }
